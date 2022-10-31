7af9267f277291555dab5a4973fc0d0f5fdd16de
Robert Spier <robert.spier@gmail.com>
2022-10-31 11:50:43 +0100

    Add boilerplate

---
+++ week-2/boilerplate/scripts/main.js
@@ -0,0 +1,7 @@
+// Our bundler automatically creates styling when imported in the main JS file!
+import '../styles/style.css'
+
+// We can use node_modules directely in the browser!
+import * as d3 from 'd3';
+
+console.log('Hello, world!');
