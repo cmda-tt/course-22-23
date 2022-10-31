09c33019dd800d313957e05b52f6854150d306fc
Robert Spier <robert.spier@gmail.com>
2022-10-31 15:27:32 +0100

    Add boilerplate

---
+++ week-2/boilerplate/scripts/count.js
@@ -0,0 +1,3 @@
+export default function count(number1, number2) {
+	return number1 + number2;
+}
--- week-2/boilerplate/scripts/main.js
+++ week-2/boilerplate/scripts/main.js
@@ -3,5 +3,10 @@ import '../styles/style.css'
 
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
+import count from './count.js';
 
 console.log('Hello, world!');
+
+let data = count(5, 5);
+
+console.log(data);
